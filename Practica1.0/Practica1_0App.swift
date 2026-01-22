b23aee6fd0cc2dfe7a1bef2bb66557995e21fa21
Mariana Lamego Soriano <m.lamego.soriano@outlook.com>
2026-01-22 12:58:07 -0600

    Initial Commit

---
+++ Practica1.0/Practica1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Practica1_0App.swift
+//  Practica1.0
+//
+//  Created by ulab on 22/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Practica1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
